20097d84dcc304b6f7815af4740c222724994da7
getdigitaltransformation007 <getdigitaltransformation007@gmail.com>
2021-02-17 19:10:21 +0530

    First Commit Init PRoject Setup

---
+++ app/Http/routes.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web','cors']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    Route::get('/sample', function (Request $request) {
+        $request->header('Access-Control-Allow-Origin', '*');
+        $request->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $request->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        $getSampleData = Task::orderBy('created_at', 'asc')->get();
+        return Response::json(["data"=>$getSampleData]);
+    });
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+});
